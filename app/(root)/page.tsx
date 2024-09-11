f21182927d0354ee353e2c218026919fe9430b54
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2024-09-11 22:59:37 +0530

    initialize routes and home pages

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <section>
+            <div className="home">
+                <div className="home-content">
+                    <header className="home-header">
+                        Welcome Home
+                    </header>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Home;
