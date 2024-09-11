63b68a99d04389c77a2e01144a778459b0610c9b
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2024-09-11 23:49:33 +0530

    added reusable header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import HeaderBox from "@/components/headerBox";
 
 const Home = () => {
+    const loggedIn = { firstName: "User" }
     return (
         <section>
             <div className="home">
                 <div className="home-content">
                     <header className="home-header">
-                        Welcome Home
+                        <HeaderBox
+                            type="greeting"
+                            title="Welcome"
+                            user={loggedIn?.firstName || "Guest"}
+                            subtext="Access and manage your account and transactions efficiently."
+                        />
                     </header>
                 </div>
             </div>
